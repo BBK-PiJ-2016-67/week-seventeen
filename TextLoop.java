e30c045eee889153b5978af94b686d1e03b0710b
lucas <lucasjgordon@gmail.com>
2017-02-20 19:23:43 +0000

    Adds sleep method.

--- TextLoop.java
+++ TextLoop.java
@@ -7,7 +7,19 @@ public class TextLoop implements Runnable {
 	@Override
 	public void run() {
 		for (int i = 0; i < COUNT; i++) {
-			System.out.println("Loop:" + name + ", iteration:" + i + ".");
+			sleep();
+		}
+	}
+	private void sleep() {
+		try {
+			System.out.print("Sleeping for a second");
+			for (int i = 0; i < 5; i++) {
+				Thread.sleep(200);
+				System.out.print(".");
+			}
+			System.out.println("done!");
+		} catch (InterruptedException ex) {
+			ex.printStackTrace(); // Nothing to do, sleep a bit less
 		}
 	}
 	public static void main(String args[]) {
